f9220156c5996cad984c38bad9f9bcd3ad286176
Christian Shaw <cshaw00048@gmail.com>
2020-08-18 04:44:02 -0500

    nested for loop

--- ticTacToe.js
+++ ticTacToe.js
@@ -11,3 +11,11 @@ function setup(){
     createCanvas(400, 400);
 }
 
+function draw() {
+    background (220);
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; i < 3; i++) {
+            
+        }
+    } 
+}
