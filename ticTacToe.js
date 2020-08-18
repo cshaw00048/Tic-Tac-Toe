2c9a03c85bf63519edac5577182b1f28dbe16f9d
Christian Shaw <cshaw00048@gmail.com>
2020-08-18 05:03:46 -0500

    fixing Xs on the board

--- ticTacToe.js
+++ ticTacToe.js
@@ -15,13 +15,23 @@ function draw() {
     background (220);
     let w = width / 3;
     let h = height / 3;
+
     for (let i = 0; i < 3; i++) {
-        for (let j = 0; i < 3; i++) {
-            let x = w * i;
-            let y = h * j;
+        for (let j = 0; j < 3; j++) {
+            let x = w * i + w/2;
+            let y = h * j + h/2;
             let spot = board [i][j];
             textSize(32);
-            text(spot, x, y);
+            if (spot == player2) {
+                noFill();
+                ellipseMode(CORNER);
+                ellipse(x,y,w);
+            }else if  (spot == player1){
+                let xr = w/4;
+                line (x - xr, y - xr, x + xr, y + xr);
+                line (x + xr, y - xr, x - xr, y + xr);
+            }
+            
         }
     } 
 }
