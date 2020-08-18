fa7539d1040dd919e1bace1ac751e98d79af9c9b
Christian Shaw <cshaw00048@gmail.com>
2020-08-18 05:16:02 -0500

    finished changes to players array and increased background size

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,41 +1,41 @@
 let board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', ''],
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
 ];
 
-let players = ['X', 'O'];
+let players = ["X", "O"];
 
 let currentPlayer;
-function setup(){
-    createCanvas(400, 400);
-    if (random(1) < 0.5) {
-    currentPlayer = player1;
-} else {
-
+function setup() {
+  createCanvas(400, 400);
+  if (random(1) < 0.5) {
+    currentPlayer = players[0];
+  } else {
+    currentPlayer = players[1];
+  }
 }
 
 function draw() {
-    background (220);
-    let w = width / 3;
-    let h = height / 3;
+  background(255);
+  let w = width / 3;
+  let h = height / 3;
 
-    for (let j = 0; j < 3; j++) {
-        for (let i = 0; i < 3; i++) {
-            let x = w * i + w/2;
-            let y = h * j + h/2;
-            let spot = board [i][j];
-            textSize(32);
-            strokeWeight(4);
-            if (spot == player2) {
-                noFill();
-                ellipse(x,y,w/2);
-            }else if  (spot == player1){
-                let xr = w/4;
-                line (x - xr, y - xr, x + xr, y + xr);
-                line (x + xr, y - xr, x - xr, y + xr);
-            }
-            
-        }
-    } 
+  for (let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++) {
+      let x = w * i + w / 2;
+      let y = h * j + h / 2;
+      let spot = board[i][j];
+      textSize(32);
+      strokeWeight(4);
+      if (spot == players[1]) {
+        noFill();
+        ellipse(x, y, w / 2);
+      } else if (spot == players[0]) {
+        let xr = w / 4;
+        line(x - xr, y - xr, x + xr, y + xr);
+        line(x + xr, y - xr, x - xr, y + xr);
+      }
+    }
+  }
 }
