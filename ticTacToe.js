11805b6f60fd430354fe23622583c83cc0b08280
Christian Shaw <cshaw00048@gmail.com>
2020-08-18 14:05:36 -0500

    declaring winner

--- ticTacToe.js
+++ ticTacToe.js
@@ -19,34 +19,38 @@ function setup() {
   }
 }
 
+function equals3(a, b, c) {
+    return (a==b && b==c && a != '');
+}
+
 function checkWinner() {
    let winner = null;
 
    //horizontal
     for (let i = 0; i < 3; i++){
-        if (board[i][0] == board[i][1] == board[i][2]){
+        if (equals3(board[i][0], board[i][1], board[i][2])){
             winner = board[i][0];
         }
     }
     //vertical
     for (let i = 0; i < 3; i++){
-        if (board[0][i] == board[0][i] == board[0][i]){
+        if (equals3(board[0][i], board[1][i], board[2][i])){
             winner = board[0][i];
         }
     }
     //diagonal
-    if(board[0][0] == board [1][1] == board[2][2]){
+    if(equals3(board[0][0], board [1][1], board[2][2])){
         winner = board [0][0];
     }
 
-    if(board[2][0] == board [1][1] == board[0][2]){
+    if(equals3(board[2][0], board [1][1], board[0][2])){
         winner = board [2][0];
     }
 
    if (winner == null && available.length == 0){
-       console.log('tie');
+       return('tie');
    } else {
-       console.log(winner);
+       return winner;
    }
 }
 
@@ -90,6 +94,13 @@ function draw() {
       }
     }
   }
+
+  let result = checkWinner();
+  if (result != null) {
+      noLoop();
+      createP(result).style('color', '#FFF', '32pt');
+      console.log(result);
+  }
+
   nextTurn();
-  checkWinner();
 }
